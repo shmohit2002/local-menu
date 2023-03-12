45dbc15be34f19e8bcb4500c3e2195bcc71ca64f
Mohit Sharma <shmohit2002@gmail.com>
2023-03-12 22:46:32 +0530

    update button

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
-
+import {Grid,}
 function Menu() {
     return (
-        <div>
-            Hello
-        </div>
+        <Grid templateColumns='repeat(2, 1fr)'>
+            <GridItem><Card maxW='xs'>
+                <CardHeader>
+                    <Flex spacing='4'>
+                        <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+                            <Box>
+                                <Heading size='sm'>Sample Name</Heading>
+                                <Text>Last Updated: 11/03/2023</Text>
+                            </Box>
+                        </Flex>
+                    </Flex>
+                </CardHeader>
+                <CardBody>
+                    <Text>
+                        Lorem
+                    </Text>
+                </CardBody>
+                <Image
+                    objectFit='cover'
+                    src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                    alt='Chakra UI'
+                />
+
+                <CardFooter
+                    justify='space-between'
+                    flexWrap='wrap'
+                    sx={{
+                        '& > button': {
+                            minW: '136px',
+                        },
+                    }}
+                >
+                    <Button flex='1' variant='ghost' leftIcon={<BiLike />}>
+                        Update
+                    </Button>
+                    <Button flex='1' variant='ghost' leftIcon={<BiChat />}>
+                        Comment
+                    </Button>
+                    <Button flex='1' variant='ghost' leftIcon={<BiShare />}>
+                        Share
+                    </Button>
+                </CardFooter>
+            </Card>
+            </GridItem>
+        </Grid>
     )
 }
 
